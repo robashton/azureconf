7b6b0f40ca8b6ead77ba86fc04ba9cdb03eed5af
Rob Ashton <robashton@codeofrob.com>
2013-04-22 16:50:54 -1000

    Back to blank slate

--- server.js
+++ server.js
@@ -1,18 +1,9 @@
 var express = require('express')
-  , stylus = require('stylus')
 
 var app = express()
 
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jade')
-app.use(stylus.middleware({
-   src: __dirname + '/site'
-}))
-app.use(express.static(__dirname + '/site'))
-
-app.listen(process.env.PORT || 8003)
+app.listen(8003)
 
 app.get('/', function(req, res) {
-  res.render('index')
+  res.send('hello world')
 })
-
